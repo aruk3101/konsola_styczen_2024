3744e7195197a2ff2e7aa4ad12fddda8f22493da
Damian Raczek <draczekprojekt4206695@gmail.com>
2024-10-04 09:55:37 +0200

    Most of the functions implemented, 1 bug still existing

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,20 +1,46 @@
-﻿namespace ConsoleApp1
+﻿using System.Security.Cryptography;
+
+namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Podaj swoj pesel: ");
             string pesel = "55030101193";
+            string buffer = Console.ReadLine();
+            pesel = String.IsNullOrEmpty(buffer) == true ? "55030101193" : buffer;
+            Console.Write("Twoja plec to: ");
+            Console.WriteLine(SprawdzPlec(pesel) == 'K' ? "Kobieta" : "Męszczyna");
+            
+            Console.WriteLine(SprawdzSumeKontrolna(pesel) == true ? "Twoj pesel jest poprawny" : "Twoj pesel jest niepoprawny");
         }
 
         public static char SprawdzPlec(string pesel)
         {
-            return 'K';
+            return Convert.ToInt32(pesel[9]) % 2 == 0 ? 'K' : 'M';
         }
 
         public static bool SprawdzSumeKontrolna(string pesel)
         {
-            return true;
+            List<int> wagi = new List<int>()
+            {
+                1,3,7,9,1,3,7,9,1,3
+            };
+            int S = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                int liczba = Convert.ToInt32(pesel[i] - '0');
+                S += liczba * wagi[i];
+            }
+            int M = S % 10;
+            int R = 0;
+            if (M == 0) R = 0;
+            else
+            {
+                R = 10 - M;
+            }
+            return (R == pesel[10]) ? false : true;
         }
     }
 }
